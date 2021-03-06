1684d206a8ab501fc9dc79c25e0cfdc18f010669
Elsee Sea <sub01+omen@iaonline.net>
2021-03-06 17:52:26 -0600

    Added Hello controller from Ch. 10.1.2

---
+++ Controllers/HelloController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HelloLih.Controllers
+{
+    public class HelloController : Controller
+    {
+        /*
+        public IActionResult Index()
+        {
+            return View();
+        }
+        */
+
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            string html = "<h1>" + "Hello World!" + "<h1>";
+            return Content(html, "text/html");
+        }
+    }
+}
